08668cd70566467a035a3c8e4d0fd416400c517a
Dmitriy Korchagin <dmitriy.korchagin95@icloud.com>
2026-09-15 09:21:53 +0300

    Use test profile in app tests

--- src/test/java/hexlet/code/controller/api/UsersControllerTest.java
+++ src/test/java/hexlet/code/controller/api/UsersControllerTest.java
@@ -25,9 +25,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc(addFilters = false)
 class UsersControllerTest {
 
